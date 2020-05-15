7ab8c8834b66353934132463a77034c977feab9c
pranoy panda <1997pranoy@gmail.com>
2020-05-15 20:34:54 +0000

    trainer.py edited online with Bitbucket

--- trainer.py
+++ trainer.py
@@ -46,7 +46,7 @@ def train_model(model, criterion, dataloaders, optimizer, metrics, bpath, num_ep
                 # current mask has dim: (batch_size,1,W,H) and I want it to be (batch_size,W,H)
                 masks = masks.squeeze()
                 
-                #print(masks.shape)
+                print(masks.shape)
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
